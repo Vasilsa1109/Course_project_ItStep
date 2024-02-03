b06127001e3638ebde04f022ed6d783e5d034e1a
Vasilisa <vasilisavygovskaya1109@gmail.com>
2024-02-03 17:41:30 +0300

    --home-changes-add cont2 + description

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 let m = document.querySelector(".menu_icon");
 let nav = document.querySelector(".main-menu");
+let cont2 =  document.querySelector(".cont2");
 
 if(m){
     m.addEventListener("click", (e) => {
         if(m.classList.contains("pushed")){
             m.classList.remove("pushed");
             nav.classList.remove("opened");
+            cont2.classList.remove("hidden");
         }
         else{
             m.classList.add("pushed");
             nav.classList.add("opened");
+            cont2.classList.add("hidden");
         }
     });
 }
